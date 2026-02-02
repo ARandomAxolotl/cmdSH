6e41aadc47feffc650701e590eb5626859c5a3bc
ARandomAxolotl <pmminhtriet2013@gmail.com>
2026-02-02 22:29:40 +0700

    fixed some random bugs with func

--- Functions/func.cmd
+++ Functions/func.cmd
@@ -28,7 +28,6 @@ set "target=%~2"
 
 if /i "!cmd!" == "install"        goto :install
 if /i "!cmd!" == "uninstall"      goto :uninstall
-if /i "!cmd!" == "reinstall"      goto :reinstall
 if /i "!cmd!" == "list"           goto :list
 if /i "!cmd!" == "update"         goto :update
 if /i "!cmd!" == "add-repo"       goto :addrepo
@@ -71,12 +70,16 @@ if %errorlevel% == 0 (
 goto :end
 
 :search
-if "!target!" == "" ( echo %ESC%[31m[Error]%ESC%[0m Specify a keyword. & goto :end )
+if "!target!" == "" (
+    echo %ESC%[31m[Error]%ESC%[0m Specify a keyword.
+    goto :end
+)
 echo %ESC%[36mSearching for "!target!"...%ESC%[0m
 for %%F in ("!saveDir!\*.txt") do (
-    for /f "tokens=4 delims=:," %%G in ('findstr /i "name" "%%F" ^| grep -i "!target!"') do (
-        set "raw=%%~G" & set "clean=!raw: ="!" & set "clean=!clean:"=!"
-        if /i "!clean:~-4!" == ".cmd" echo  - !clean:.cmd=! %ESC%[90m(In %%~nxF)%ESC%[0m
+    if exist "%%F" (
+        for /f "usebackq tokens=*" %%A in (`findstr /i "!target!" "%%F" 2^>nul`) do (
+            echo  - %%A %ESC%[90m(In %%~nxF)%ESC%[0m
+        )
     )
 )
 goto :end
@@ -84,32 +87,70 @@ goto :end
 :update
 echo %ESC%[36mUpdating Caches...%ESC%[0m
 for /f "usebackq tokens=*" %%U in (`findstr /V /B "#" "!configPath!"`) do (
-    set "api=%%U"
-    set "api=!api:raw.githubusercontent.com=api.github.com/repos!"
+    set "url=%%U"
+    set "api=!url:raw.githubusercontent.com=api.github.com/repos!"
     set "api=!api:/main/=/contents/!"
     set "api=!api:/master/=/contents/!"
-    set "sName=%%U" & set "sName=!sName::=!" & set "sName=!sName:/=_!"
-    echo Fetching: %%U
-    curl -s "!api!" > "!saveDir!\!sName!.txt"
+    
+    :: Clean name for local storage
+    set "sName=%%U"
+    set "sName=!sName:https://=!"
+    set "sName=!sName:http://=!"
+    set "sName=!sName:/=_!"
+    set "sName=!sName::=!"
+    
+    :: Bazzite/Fedora style orbit spinner
+    set "spinChars=⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+    set "idx=0"
+
+    echo %ESC%[33mFetching: %%U%ESC%[0m
+    curl -L -H "User-Agent: cmdSH" -s "!api!" > "!saveDir!\temp.json"
+    
+    if exist "!saveDir!\!sName!.txt" del "!saveDir!\!sName!.txt"
+    
+    :: Logic to split giant JSON lines into a clean list
+    for /f "usebackq delims=" %%L in ("!saveDir!\temp.json") do (
+        set "line=%%L"
+        set "line=!line:,= !"
+        for %%A in (!line!) do (
+            set "item=%%~A"
+            
+            :: Update the Bazzite-style Spinner
+            set /a "idx=(idx+1)%%10"
+            for %%i in (!idx!) do set "char=!spinChars:~%%i,1!"
+            <nul set /p "=%ESC%[s%ESC%[32m!char! %ESC%[37mProcessing...%ESC%[u"
+
+            echo !item! | findstr /i "name" >nul
+            if !errorlevel! == 0 (
+                for /f "tokens=2 delims=:" %%B in ("!item!") do (
+                    set "clean=%%~B"
+                    if /i "!clean:~-4!" == ".cmd" (
+                        set "final=!clean:.cmd=!"
+                        echo !final!>>"!saveDir!\!sName!.txt"
+                    )
+                )
+            )
+        )
+    )
+    echo %ESC%[K%ESC%[32m✔ Done.%ESC%[0m
+    if exist "!saveDir!\temp.json" del "!saveDir!\temp.json"
+)
+:list
+if /i "!target!" == "--installed" goto :list_installed
+echo %ESC%[36mAvailable (Cached):%ESC%[0m
+for %%F in ("!saveDir!\*.txt") do (
+    echo %ESC%[33mSource: %%~nxF%ESC%[0m
+    if exist "%%F" (
+        for /f "usebackq tokens=*" %%A in ("%%F") do echo  - %%A
+    )
 )
 goto :end
 
-:list
-if /i "!target!" == "--installed" (
-    echo %ESC%[36mInstalled:%ESC%[0m
-    for %%F in ("!funcDir!*.cmd") do (
-        for %%A in ("%%F") do set "fTime=%%~tA"
-        echo  - %%~nF %ESC%[90m(!fTime!)%ESC%[0m
-    )
-) else (
-    echo %ESC%[36mAvailable (Cached):%ESC%[0m
-    for %%F in ("!saveDir!\*.txt") do (
-        echo %ESC%[33mSource: %%~nxF%ESC%[0m
-        for /f "tokens=4 delims=:," %%G in ('findstr /i "name" "%%F"') do (
-            set "raw=%%~G" & set "clean=!raw: ="!" & set "clean=!clean:"=!"
-            if /i "!clean:~-4!" == ".cmd" echo  - !clean:.cmd=!
-        )
-    )
+:list_installed
+echo %ESC%[36mInstalled:%ESC%[0m
+for %%F in ("!funcDir!*.cmd") do (
+    set "fTime=%%~tF"
+    echo  - %%~nF %ESC%[90m[!fTime!]%ESC%[0m
 )
 goto :end
 
